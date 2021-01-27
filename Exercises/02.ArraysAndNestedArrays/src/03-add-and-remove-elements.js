ca0b273335d6713ab24eac5b70aa8d113411103e
Valkov <vulkov.borislav@gmail.com>
2021-01-27 20:19:28 +0200

    Add 03-add-and-remove-elements

---
+++ Exercises/02.ArraysAndNestedArrays/src/03-add-and-remove-elements.js
@@ -0,0 +1,19 @@
+function solve(commands) {
+    const arr = [];
+
+    const commandParser = {
+        add(value) { arr.push(value); },
+        remove() { arr.pop(); }
+    }
+
+    for (let i = 0; i < commands.length; i++) {
+        commandParser[commands[i]](i + 1);
+    }
+
+    const result = arr.length != 0 ? arr.join('\n') : 'Empty';
+
+    return result;
+}
+
+console.log(solve(['add', 'add', 'add', 'add']));
+console.log(solve(['add', 'add', 'remove', 'add', 'add']));
